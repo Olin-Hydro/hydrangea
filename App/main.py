d1842b0757220ffc2cf5e2a61d7b38f5dd89e894
jonaskaz <jonas.kazlauskas@me.com>
2023-02-27 14:46:28 -0500

    Remove beanstalk and add lambda deployment

--- App/main.py
+++ App/main.py
@@ -3,10 +3,15 @@ import os
 import uvicorn
 
 from dotenv import load_dotenv
+from server import app
+from mangum import Mangum
+
 
 load_dotenv()
 
 port = int(os.environ["PORT"])
+
+handler = Mangum(app)
 
 if __name__ == "__main__":
     uvicorn.run("server.app:app", host="0.0.0.0", port=port, reload=False)
