c881ee445ff2d3edef2662555fb4e0cb7d7b64a3
Daeyoung Kim <corydykim@gmail.com>
2023-04-19 17:31:55 -0400

    Merge with upstream main

--- app/routes/command.py
+++ app/routes/command.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, Body, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
 
-from app.models.command import Command
+from app.models.command import Command, CommandUpdate
 
 
 router = APIRouter()
@@ -30,8 +30,11 @@ def create_command(request: Request, commands: List[Command] = Body(...)):
 @router.get(
     "/", response_description="List commands", response_model=List[Command]
 )
-def list_commands(request: Request, limit: int = 1000):
-    commands = list(request.app.database["commands"].find())
+def list_commands(request: Request, limit: int = 1000, executed="false"):
+    commands = list(
+        request.app.database["commands"].find({"executed": executed})
+    )
+
     commands.sort(key=lambda r: r["updated_at"], reverse=True)
     return commands[:limit]
 
@@ -46,6 +49,34 @@ def find_command(id: str, request: Request):
         command := request.app.database["commands"].find_one({"_id": id})
     ) is not None:
         return command
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Command with ID {id} not found",
+    )
+
+
+@router.put(
+    "/{id}", response_description="Update a command", response_model=Command
+)
+def update_command(id: str, request: Request, cmd: CommandUpdate = Body(...)):
+    cmd = {k: v for k, v in cmd.dict().items() if v is not None}
+
+    if len(cmd) >= 2:
+        update_result = request.app.database["commands"].update_one(
+            {"_id": id}, {"$set": cmd}
+        )
+
+        if update_result.modified_count == 0:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Nothing was updated",
+            )
+
+    if (
+        existing_cmd := request.app.database["commands"].find_one({"_id": id})
+    ) is not None:
+        return existing_cmd
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
